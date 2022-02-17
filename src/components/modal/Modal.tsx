b00912dc083873ccd7d2ae1e313306b0665fa9db
Gentjana Demnushaj <gdemnushaj@gmail.com>
2022-02-17 21:49:35 +0000

    styling tweaks

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -19,7 +19,7 @@ const OVERLAY_STYLES = {
   left: 0,
   right: 0,
   bottom: 0,
-  backgroundColor: "rgba(0, 0, 0, .65)",
+  backgroundColor: "rgba(0, 0, 0, .7)",
   zIndex: 1000,
 } as React.CSSProperties;
 
